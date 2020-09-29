e8d77a890e0066639710d843d5a5e5922293e53d
pournaki <pournaki@mis.mpg.de>
2020-09-29 16:46:29 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# houseofcommons-parser
-Parser for the UK House of Commons Hansard Reports
+# UK House of Commons Parser
+
+## Description
+A parser that transforms speeches from the [UK House of Commons](https://www.parliament.uk/business/commons/) to a list of JSONs. The raw XML data is retrieved from [ParlParse](http://parser.theyworkforyou.com/hansard.html). 
+
+The speeches are saved in `./speeches.jsonl`. Every line consists of one speech dictionary. See [sample output](#sample-output) for more information. 
+
+## How to use
+Clone and enter this repo:
+``` 
+$ git clone https://github.com/pournaki/houseofcommons-parser
+$ cd houseofcommons-parser
+```
+
+Install the necessary libraries: 
+
+``` 
+$ pip3 install -r requirements.txt
+```
+
+Note that this repo does not contain the raw data. You need to retrieve the XMLs by year to the speeches directory from [ParlParse](http://parser.theyworkforyou.com/hansard.html) using rsync:
+
+```
+$ cd data
+$ mkdir speeches
+$ cd speeches
+$ rsync -az --progress --exclude '.svn' --exclude 'tmp/' "data.theyworkforyou.com::parldata/scrapedxml/debates/debatesYEAR-*" .
+```
+
+For example, if you want the XMLs for 2019, run this command from within `./data/speeches`:
+```
+$ rsync -az --progress --exclude '.svn' --exclude 'tmp/' "data.theyworkforyou.com::parldata/scrapedxml/debates/debatesYEAR-*" .
+```
+
+Once you have the XMLs, go back to the parent folder and run the parser:
+```  
+$ cd ../../
+$ python3 run.py
+```
+
+## Sample output
+```
+{
+  "id": "uk.org.publicwhip/debate/2018-07-02b.117.0",
+  "speaker_id": "uk.org.publicwhip/person/10045",
+  "date": "2018-07-02",
+  "name": "Clive Betts",
+  "party": "labour",
+  "text": "I thank the hon. Gentleman for the part he played in the joint Select Committee report. He is absolutely right about that. Figures in the inquiry showed that for the same work, social care workers were paid about 29% less on average than workers in the NHS.",
+  "discussion_title": "MINISTRY OF JUSTICE 2018-07-02"
+ }
+```
---
+++ src/parser.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+
+"""Parser for UK House of Commons Data
+
+Transforms HoC speeches from XML 
+to json list file.
+"""
+
+__author__ = "Armin Pournaki"
+__copyright__ = "Copyright 2020, Armin Pournaki"
+__license__ = "GPLv3"
+__version__ = "1.0"
+__email__ = "pournaki@mis.mpg.de"
+
+
+def parser():
+
+    def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+        percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+        filledLength = int(length * iteration // total)
+        bar = fill * filledLength + '-' * (length - filledLength)
+        print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+        # Print New Line on Complete
+        if iteration == total: 
+            print()
+
+    # load libraries
+    from lxml import etree
+    import nested_lookup as nl
+    import os
+    import json
+
+    
+    DATADIR = "./data/speeches/"
+    # get all the filenames in the directory
+    filenamelist = []
+    def absoluteFilePaths(DATADIR):
+        for dirpath,_,filenames in os.walk(DATADIR):
+            for f in filenames:
+                filenamelist.append(os.path.join(dirpath, f))
+        return filenamelist
+    filenamelist = sorted(absoluteFilePaths(DATADIR))
+
+    with open("./data/people.json") as json_file:
+        memberdata = json.load(json_file)
+    
+    jsondict     = {}
+
+    l = len(filenamelist)
+    
+    # Initial call to print 0% progress
+    printProgressBar(0, l, prefix = 'Parsing...', suffix = 'Complete', length = 50)
+    
+    for progresso, filename in enumerate(filenamelist):
+
+        parser = etree.XMLParser(dtd_validation=False)
+        tree = etree.parse(filename, parser)
+        root = tree.getroot()
+
+        printProgressBar(progresso + 1, l, prefix = 'Parsing...', suffix = 'Complete', length = 50)
+
+        context = root.findall('./')
+
+        for element in context:
+    
+            if element.tag == 'major-heading':
+                headingtext = element.text
+                headingtext = headingtext.replace("\n","").replace("\t","")
+                date = filename[39:49]
+                jsondict[f"{headingtext} {date}"] = {}
+                jsondict[f"{headingtext} {date}"]["speeches"] = []
+            
+            if "headingtext" in locals():
+
+                if element.tag == 'speech':    
+                    speakername = element.attrib.get("speakername")
+                    speakerid   = element.attrib.get("person_id")
+                    speechid    = element.attrib.get("id")
+
+                    speechtext = str()
+                    
+                    for t in element.getchildren():
+                        if t.getchildren() == []:
+                            try:
+                                speechtext += t.text
+                            except TypeError:
+                                pass
+                        else:
+                            for p in t.getchildren():
+                                try:
+                                    speechtext += t.text
+                                    speechtext += p.text
+                                    speechtext += p.tail
+                                    speechtext += " "
+                                except TypeError:
+                                    pass
+
+
+                    speechdict = {}
+
+                    try:
+                        speechdict["id"] = speechid
+                    except KeyError:
+                        pass
+                    try:
+                        speechdict["speaker_id"] = speakerid
+                    except KeyError:
+                        speechdict["speaker_id"] = "None"
+                    try:
+                        speechdict["date"] = date
+                    except KeyError:
+                        speechdict["date"] = "None"  
+                    try:
+                        speechdict["name"] = speakername
+                    except KeyError:
+                        speechdict["name"] = "None"
+                    try:
+                        speechdict["party"]  = memberdata[speakerid]
+                    except KeyError:
+                        speechdict["party"] = "None"
+                    try:
+                        speechdict["text"]           = speechtext
+                    except KeyError:
+                        pass
+                    try:
+                        jsondict[f"{headingtext} {date}"]["speeches"].append(speechdict)
+                    except KeyError:
+                        pass
+
+                    if speechdict["name"] == None:
+                        speechdict["name"] = "None"
+                    if speechdict["speaker_id"] == None:
+                        speechdict["speaker_id"] = "None"
+                    if speechdict["party"] == None:
+                        speechdict["party"] = "None"
+                    
+    
+    parsed_speeches = jsondict
+
+    for element in parsed_speeches:
+        for speech in parsed_speeches[element]["speeches"]:
+            speech["discussion_title"] = element
+
+    flatdict = []
+    for element in parsed_speeches:
+        for speech in parsed_speeches[element]["speeches"]:
+            flatdict.append(speech)
+
+    for speeches in flatdict:
+        speeches["text"] = speeches["text"].replace('"', '``').replace("'",'`')
+
+        try:
+            speeches["name"] = speeches["name"].replace('"', '``').replace("'",'`')
+        except AttributeError:
+            pass
+    
+    with open ("./speeches.jsonl", "w", encoding = "utf-8") as f:
+        for line in flatdict:
+            json.dump(line, f, ensure_ascii=False)
+            f.write("\n")
+
+    print("Success! Saved parsed speeches to './speeches.jsonl'.")
+
+    return flatdict
+
+
+    # with open(f"./speeches_nested.json","w", encoding='utf-8') as f:
+    #     json.dump(jsondict,f,ensure_ascii=False,indent = 0)
+     
+    # return jsondict
